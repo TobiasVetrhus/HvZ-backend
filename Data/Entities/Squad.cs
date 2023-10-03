ff76f233313cb4ebf01a5e150afcf9684f50778d
Ritwaan <140506214+Ritwaan@users.noreply.github.com>
2023-10-03 22:05:24 +0200

    Squad Model

---
+++ Data/Entities/Squad.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HvZ_backend.Data.Entities
+{
+    public class Squad
+    {
+        public int Id { get; set; }
+
+        [StringLength(100)]
+        public string SquadName { get; set; }
+
+        public int? NumberOfMembers { get; set; }
+
+        public int? NumberOfDeceased { get; set; }
+    }
+}
+
