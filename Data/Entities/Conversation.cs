7e2c40d1afcc6a4e76073d1d6d91d8d2334b5931
Tobias Vetrhus <tobias-vetrhus@outlook.com>
2023-10-03 15:37:02 +0200

    Conversation model made

---
+++ Data/Entities/Conversation.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HvZ_backend.Data.Entities
+{
+    public enum ChatType
+    {
+        Global,
+        Squad,
+        FactionZombies,
+        FactionHumans
+    }
+    public class Conversation
+    {
+        public int Id { get; set; }
+        [StringLength(50)]
+        public string ConversationName { get; set; }
+        public ChatType ChatType { get; set; }
+        public int GameId { get; set; }
+
+        //Navigation
+        public Game game { get; set; }
+        public ICollection<Player> Players { get; set; } //M-M
+
+    }
+}
