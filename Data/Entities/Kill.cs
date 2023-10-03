f5bab31fbb0228d145f6b445fc99abdd0ea950d5
Tobias Vetrhus <tobias-vetrhus@outlook.com>
2023-10-03 15:00:43 +0200

    Fixed Kill Entity

--- Data/Entities/Kill.cs
+++ Data/Entities/Kill.cs
@@ -1,25 +1,22 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HvZ_backend.Data.Entities
 {
     public class Kill
     {
-        [Key]
-        public int Id { get; set; } 
-
-        [StringLength(255)] 
+        public int Id { get; set; }
+        [StringLength(255)]
         public string Description { get; set; }
-
-        [Required]
         [Column(TypeName = "timestamp")]
-        public DateTime TimeOfKill { get; set; } 
+        public DateTime TimeOfKill { get; set; }
+        public int KillerPlayerId { get; set; }
+        public int VictimPlayerId { get; set; }
 
-        [Required]
-        [ForeignKey("PlayerId")]
-        public int PlayerId { get; set; } 
+        //Navigation to access the killer and victim players
+        public Player KillerPlayer { get; set; }
+        public Player VictimPlayer { get; set; }
 
-  
+
     }
 }
