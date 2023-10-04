b7931f6d3720d0ece1d21e29c040156022a5aa49
Tobias Vetrhus <tobias-vetrhus@outlook.com>
2023-10-04 08:21:30 +0200

    Made ICrudService.cs

---
+++ Services/ICrudService.cs
@@ -0,0 +1,10 @@
+﻿namespace HvZ_backend.Services
+{
+    public interface ICrudService<TEntity, ID>
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> GetByIdAsync(ID id);
+        Task<TEntity> UpdateAsync(TEntity obj);
+        Task<TEntity> DeleteByIdAsync(ID id);
+    }
+}
