f9a93932459e0559d6e807d1d52e64887f5c898c
Tobias Vetrhus <tobias-vetrhus@outlook.com>
2023-10-04 11:21:39 +0200

    Made Squad DTOs

---
+++ Data/DTOs/Squads/SquadPostDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HvZ_backend.Data.DTOs.Squads
+{
+    public class SquadPostDTO
+    {
+        [StringLength(100)]
+        public string SquadName { get; set; }
+        public int? NumberOfMembers { get; set; }
+        public int? NumberOfDeceased { get; set; }
+        public int[]? PlayerIds { get; set; }
+    }
+}
---
+++ Data/DTOs/Squads/SquadPutDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HvZ_backend.Data.DTOs.Squads
+{
+    public class SquadPutDTO
+    {
+        public int Id { get; set; }
+        [StringLength(100)]
+        public string SquadName { get; set; }
+        public int? NumberOfMembers { get; set; }
+        public int? NumberOfDeceased { get; set; }
+        public int[]? PlayerIds { get; set; }
+    }
+}
