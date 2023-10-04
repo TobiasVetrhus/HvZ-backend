6abc52047c5020a6ff99f6626ea606ad0a075f98
Tobias Vetrhus <tobias-vetrhus@outlook.com>
2023-10-04 11:45:12 +0200

    Made Mission DTOs

---
+++ Data/DTOs/Missions/MissionDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace HvZ_backend.Data.DTOs.Missions
+{
+    public class MissionDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int LocationId { get; set; }
+        public int GameId { get; set; }
+    }
+}
