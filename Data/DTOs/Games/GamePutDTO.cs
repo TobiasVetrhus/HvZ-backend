b47ad8d7a6d425b5dbd54f191c30745d24e4a995
Tobias Vetrhus <tobias-vetrhus@outlook.com>
2023-10-04 10:00:52 +0200

    Create Game DTOs

---
+++ Data/DTOs/Games/GamePutDTO.cs
@@ -0,0 +1,19 @@
+﻿using HvZ_backend.Data.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace HvZ_backend.Data.DTOs.Games
+{
+    public class GamePutDTO
+    {
+        public int Id { get; set; }
+        [StringLength(50)]
+        public string Title { get; set; }
+        [StringLength(255)]
+        public string Description { get; set; }
+        public GameStatus GameState { get; set; }
+        [StringLength(50)]
+        public string PictureURL { get; set; }
+        public int[]? RuleIds { get; set; }
+        public int[]? PlayerIds { get; set; }
+    }
+}
