f45c28a11b822c4a7c376a90faf04da813d07597
Tobias Vetrhus <tobias-vetrhus@outlook.com>
2023-10-04 14:53:25 +0200

    Added automapper and commented custom services

--- Program.cs
+++ Program.cs
@@ -1,18 +1,36 @@
 using HvZ_backend.Data.Entities;
+using HvZ_backend.Services.Games;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Swagger configuration
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<HvZDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("HvZDb"));
 });
+
+// Add custom services
+builder.Services.AddScoped<IGameService, GameService>();
+//builder.Services.AddScoped<IConversationService, ConversationService>();
+//builder.Services.AddScoped<IKillService, KillService>();
+//builder.Services.AddScoped<ILocationService, LocationService>();
+//builder.Services.AddScoped<IMessageService, MessageService>();
+//builder.Services.AddScoped<IMissionService, MissionService>();
+//builder.Services.AddScoped<IPlayerService, PlayerService>();
+//builder.Services.AddScoped<IRuleService, RuleService>();
+//builder.Services.AddScoped<ISquadService, SquadService>();
+//builder.Services.AddScoped<IUserService, UserService>();
+
+
+// Add automapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
