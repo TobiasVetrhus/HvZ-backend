bd6365fc1ad9e1517c580f9b120541bed6e6325e
Tobias Vetrhus <tobias-vetrhus@outlook.com>
2023-10-04 11:38:00 +0200

    Made Message DTOs

---
+++ Data/DTOs/Messages/MessagePostDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace HvZ_backend.Data.DTOs.Messages
+{
+    public class MessagePostDTO
+    {
+        public string Content { get; set; }
+        public DateTime Sent { get; set; }
+        public int ConversationId { get; set; }
+        public int SenderId { get; set; }
+    }
+}
