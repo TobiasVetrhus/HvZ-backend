14a06fdc717d24c9219af369cee36b9886d5c83f
Ritwaan <140506214+Ritwaan@users.noreply.github.com>
2023-10-04 08:28:17 +0200

    Mission Model Made

---
+++ Data/Entities/Mission.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HvZ_backend.Data.Entities
+{
+    public class Mission
+    {
+        public int Id { get; set; }
+
+        [StringLength(50)] 
+        public string Name { get; set; } 
+
+        [StringLength(255)]
+        public string Description { get; set; } 
+
+        public int LocationId { get; set; } 
+
+        // Navigation property 
+        public Location Location { get; set; }
+
+    }
+}
