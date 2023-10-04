a1dab8d67e2bd17920bc1e69e1967dabda5f3bc5
Tobias Vetrhus <tobias-vetrhus@outlook.com>
2023-10-04 10:42:15 +0200

    Location DTOs made

---
+++ Data/DTOs/Locations/LocationPutDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace HvZ_backend.Data.DTOs.Locations
+{
+    public class LocationPutDTO
+    {
+        public int Id { get; set; }
+        public int XCoordinate { get; set; }
+        public int YCoordinate { get; set; }
+    }
+}
