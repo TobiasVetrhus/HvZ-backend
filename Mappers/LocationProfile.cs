dcc9ec621db3e309a3dd639188b38e1b0764d89c
Tobias Vetrhus <tobias-vetrhus@outlook.com>
2023-10-06 14:52:24 +0200

    Added location mapper

---
+++ Mappers/LocationProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using HvZ_backend.Data.DTOs.Locations;
+using HvZ_backend.Data.Entities;
+
+namespace HvZ_backend.Mappers
+{
+    public class LocationProfile : Profile
+    {
+        public LocationProfile()
+        {
+            CreateMap<Location, LocationPostDTO>().ReverseMap();
+            CreateMap<Location, LocationPutDTO>().ReverseMap();
+            CreateMap<Location, LocationDTO>().ReverseMap();
+        }
+    }
+}
