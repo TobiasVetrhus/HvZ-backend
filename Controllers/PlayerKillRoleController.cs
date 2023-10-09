37fc9eeea27145ab65989937b558891df42a34fc
Tobias Vetrhus <tobias-vetrhus@outlook.com>
2023-10-09 13:35:32 +0200

    Player kill roles controller made

---
+++ Controllers/PlayerKillRoleController.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using HvZ_backend.Services.PlayerKillRoles;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HvZ_backend.Controllers
+{
+    public class PlayerKillRoleController : ControllerBase
+    {
+        private readonly IPlayerKillRoleService _playerKillRoleService;
+        private readonly IMapper _mapper;
+
+        public PlayerKillRoleController(IPlayerKillRoleService playerKillRoleService, IMapper mapper)
+        {
+            _playerKillRoleService = playerKillRoleService;
+            _mapper = mapper;
+        }
+    }
+}
