9fc5798b000edd3ae6d32bbff83348f985fdaf94
Ritwaan <140506214+Ritwaan@users.noreply.github.com>
2023-10-09 08:32:37 +0200

    Created Get rules + services.

---
+++ Mappers/RuleProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using HvZ_backend.Data.DTOs.Rules;
+using HvZ_backend.Data.Entities;
+
+namespace HvZ_backend.Mappers
+{
+    public class RuleProfile : Profile
+    {
+        public RuleProfile()
+        {
+            CreateMap<Rule, RulePostDTO>().ReverseMap();
+
+            CreateMap<Rule, RuleDTO>()
+                .ForMember(rdto => rdto.GameIds, options => options
+                    .MapFrom(r => r.Games.Select(g => g.Id).ToArray()));
+
+            CreateMap<Rule, RulePutDTO>().ReverseMap();
+        }
+    }
+}
---
+++ Services/Rules/IRuleService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HvZ_backend.Data.DTOs.Rules;
+using HvZ_backend.Data.Entities;
+
+namespace HvZ_backend.Services.Rules
+{
+    public interface IRuleService
+    {
+        Task<IEnumerable<Rule>> GetAllAsync();
+        Task<Rule> GetByIdAsync(int id);
+        Task<Rule> AddAsync(Rule rule);
+        Task<Rule> UpdateAsync(Rule rule);
+        Task DeleteByIdAsync(int id);
+    }
+}
