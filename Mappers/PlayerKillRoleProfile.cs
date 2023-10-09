704c0138348f5f3df6331dead39a68a2ffa0854e
Tobias Vetrhus <tobias-vetrhus@outlook.com>
2023-10-09 11:48:27 +0200

    Made PlayerKillRoleProfile

---
+++ Mappers/PlayerKillRoleProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using HvZ_backend.Data.DTOs.PlayerKillRoles;
+using HvZ_backend.Data.Entities;
+
+namespace HvZ_backend.Mappers
+{
+    public class PlayerKillRoleProfile : Profile
+    {
+        public PlayerKillRoleProfile()
+        {
+            CreateMap<PlayerKillRole, PlayerKillRolePostDTO>().ReverseMap();
+            CreateMap<PlayerKillRole, PlayerKillRolePutDTO>().ReverseMap();
+            CreateMap<PlayerKillRole, PlayerKillRoleDTO>().ReverseMap();
+        }
+    }
+}
