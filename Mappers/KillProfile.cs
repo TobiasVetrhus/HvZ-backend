a5b1565d61c977f1c339dd112089ca500a7a5b41
Ritwaan <140506214+Ritwaan@users.noreply.github.com>
2023-10-10 11:58:03 +0200

    Mapper for Kill

---
+++ Mappers/KillProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using HvZ_backend.Data.DTOs.Kills;
+using HvZ_backend.Data.Entities;
+
+namespace HvZ_backend.Mappers
+{
+    public class KillProfile : Profile
+    {
+        public KillProfile()
+        {
+           
+            CreateMap<Kill, KillDTO>().ReverseMap();
+ 
+            CreateMap<Kill, KillPostDTO>().ReverseMap();
+
+            CreateMap<Kill, KillPutDTO>().ReverseMap();
+        }
+    }
+}
