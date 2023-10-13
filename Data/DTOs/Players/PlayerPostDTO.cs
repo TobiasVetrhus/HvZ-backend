de29e1ed0e180d5442f0f1d92723ec01d531af7a
Tobias Vetrhus <tobias-vetrhus@outlook.com>
2023-10-13 14:29:46 +0200

    Generate bitecode, location and role in playerService

--- Data/DTOs/Players/PlayerPostDTO.cs
+++ Data/DTOs/Players/PlayerPostDTO.cs
@@ -6,13 +6,7 @@ namespace HvZ_backend.Data.DTOs.Player
     {
         [StringLength(100)]
         public string Username { get; set; }
-        public bool Zombie { get; set; }
-
-        [StringLength(8)]
-        public string BiteCode { get; set; }
         public int UserId { get; set; }
-        public int LocationId { get; set; }
-        public int SquadId { get; set; }
         public int GameId { get; set; }
     }
 }
