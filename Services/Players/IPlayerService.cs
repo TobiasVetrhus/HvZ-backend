75b481f617e4398904ac26a3c822bfc35ae46ef9
Tobias Vetrhus <tobias-vetrhus@outlook.com>
2023-10-19 20:34:44 +0200

    Rebased locationHub brnach into main

--- Services/Players/IPlayerService.cs
+++ Services/Players/IPlayerService.cs
@@ -9,10 +9,13 @@ namespace HvZ_backend.Services.Players
 =======
         Task<Player> UpdateZombieStateAsync(int playerId, bool zombie, string biteCode);
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
         Task DeletePlayerAsync(int playerId);
 >>>>>>> b0ea978 (Rebase stuff)
 >>>>>>> 7be16a6 (Rebase stuff)
+=======
+>>>>>>> 01cb3ac (locationhub)
         Task<Player> GetPlayerByBiteCodeAsync(string biteCode);
         Task<bool> updatePlayerLocationAsync(int playerId, int x, int y);
     }
