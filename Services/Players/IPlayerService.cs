e996565fbc84fb9fa1ae7e9e0c43c40c167480c1
Tobias Vetrhus <tobias-vetrhus@outlook.com>
2023-10-19 14:28:46 +0200

    Rebase stuff...

--- Services/Players/IPlayerService.cs
+++ Services/Players/IPlayerService.cs
@@ -4,7 +4,15 @@ namespace HvZ_backend.Services.Players
 {
     public interface IPlayerService : ICrudService<Player, int>
     {
+<<<<<<< HEAD
         Task<Player> UpdateZombieStateAsync(string biteCode);
+=======
+        Task<Player> UpdateZombieStateAsync(int playerId, bool zombie, string biteCode);
+<<<<<<< HEAD
+=======
+        Task DeletePlayerAsync(int playerId);
+>>>>>>> b0ea978 (Rebase stuff)
+>>>>>>> 7be16a6 (Rebase stuff)
         Task<Player> GetPlayerByBiteCodeAsync(string biteCode);
         Task<bool> updatePlayerLocationAsync(int playerId, int x, int y);
     }
